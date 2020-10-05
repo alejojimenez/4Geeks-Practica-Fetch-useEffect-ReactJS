b254288ecd4451b9c503df3791b9a42893f9e13e
alejojimenez <ajimenez.rengifo@gmail.com>
2020-10-05 20:26:49 -0300

    para revisar por @manu

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+
+  const [ taskList, setTaskList ] = useState([]);
+
+  // Efectos: consumir datos de una API
+  useEffect ( () => {
+    async function fechData() {
+      const tasks=[	
+        { 
+          "label": "Esta es mi Vigesima cuarta Tarea", 
+          "done": false 
+        },
+        { 
+          "label": "Esta es mi Vigesina quinta Tarea", 
+          "done": false 
+        },
+        { 
+          "label": "Esta es mi Vigesima sexta Tarea", 
+          "done": false 
+        }
+      ]
+
+      const additionalSetting = {
+        headers: {
+          "Content-Type": "application/json"
+        },
+        method: "PUT",
+          body: JSON.stringify(tasks)
+      }
+
+      // JSON a Text = JSON.stringify() // Text a JSON = JSON.parse()
+      fetch(
+        "https://assets.breatheco.de/apis/fake/todos/user/alejojimenez",
+        additionalSetting )
+        .then(response => response.text() )
+        .then(newResponse => {
+          console.log(newResponse);
+          setTaskList(JSON.stringify(newResponse))
+          console.log(setTaskList(JSON.stringify(newResponse)))
+        })
+        .catch((error) => console.log(error));
+    }
+    fechData();
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>To Do List Using Fetch</h1>
       </header>
     </div>
   );
